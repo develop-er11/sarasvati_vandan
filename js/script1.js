1e7a34baf994494051ea3d841663181409413adb
Rutuja Deshmukh <66289022+develop-er11@users.noreply.github.com>
2023-01-15 10:43:45 +0530

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,43 @@
+function garlanding(el, pos) {
+    /*     document.getElementById('garland').style.position="relative";
+        document.getElementById('garland').style.left="223px"; */
+
+    /* document.getElementById('garland').style.position="relative"; */
+    document.getElementById('garland').style.transition = "top 1s linear 0s";
+    document.getElementById('garland').style.top = "258px";
+
+    document.getElementById('audio');
+    audio.play();
+}
+
+function flowering(el, pos) {
+    /*  document.getElementById('zendu').style.position="relative"; */
+    document.getElementById('zendu').style.transition = "top 1s linear 0s";
+    document.getElementById('zendu').style.top = "538px";
+}
+
+function tilaking(el) {
+
+
+    document.getElementById('tilak').style.transition = "height 0.5s linear 0s";
+    document.getElementById('tilak').style.height = "24px";
+
+}
+
+function lighting(el) {
+
+
+
+    document.getElementById('flame1').style.transition = "height 0.5s linear 0s";
+    document.getElementById('flame1').style.height = "93px";
+
+}
+
+function lighting2(el) {
+
+
+
+    document.getElementById('flame2').style.transition = "height 0.5s linear 0s";
+    document.getElementById('flame2').style.height = "93px";
+
+}
